0152dff7c35d1168fed27629ea104b1848da83c8
Chris Idzerda <152215433+lal-chris@users.noreply.github.com>
2024-01-04 14:05:29 -0800

    Update the UI

--- Assets/SimpleRuntimeUI.cs
+++ Assets/SimpleRuntimeUI.cs
@@ -2,44 +2,93 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 public class SimpleRuntimeUI : MonoBehaviour {
+  private VisualElement before;
+  private VisualElement after;
+  private Toggle useBackgroundModeToggle;
+  private Button initializeButton;
+  private Label statusLabel;
   private Button startButton;
   private Button clipButton;
   private Button stopButton;
+  private TextField eventInput;
+  private Toggle useCameraToggle;
+  private Toggle useMicrophoneToggle;
+  private Button resetButton;
 
   private void OnEnable() {
     var uiDocument = GetComponent<UIDocument>();
-    startButton = uiDocument.rootVisualElement.Q("start") as Button;
-    clipButton = uiDocument.rootVisualElement.Q("clip") as Button;
-    stopButton = uiDocument.rootVisualElement.Q("stop") as Button;
+    before = uiDocument.rootVisualElement.Q("before");
+    after = uiDocument.rootVisualElement.Q("after");
+    after.style.display = new StyleEnum<DisplayStyle>(StyleKeyword.None);
+    useBackgroundModeToggle = (Toggle)uiDocument.rootVisualElement.Q("use-background-mode");
+    initializeButton = (Button)uiDocument.rootVisualElement.Q("initialize");
+    initializeButton.RegisterCallback<ClickEvent>(OnInitializeClicked);
+    statusLabel = (Label)uiDocument.rootVisualElement.Q("status");
+    startButton = (Button)uiDocument.rootVisualElement.Q("start");
+    clipButton = (Button)uiDocument.rootVisualElement.Q("clip");
+    stopButton = (Button)uiDocument.rootVisualElement.Q("stop");
     startButton.RegisterCallback<ClickEvent>(OnStartClicked);
     clipButton.RegisterCallback<ClickEvent>(OnClipClicked);
     stopButton.RegisterCallback<ClickEvent>(OnStopClicked);
-    var eventInput = uiDocument.rootVisualElement.Q("event");
-    eventInput.RegisterCallback<ChangeEvent<string>>(OnEventChanged);
+    eventInput = (TextField)uiDocument.rootVisualElement.Q("event-name");
+    useCameraToggle = (Toggle)uiDocument.rootVisualElement.Q("use-camera");
+    useMicrophoneToggle = (Toggle)uiDocument.rootVisualElement.Q("use-microphone");
+    resetButton = (Button)uiDocument.rootVisualElement.Q("reset");
+    resetButton.RegisterCallback<ClickEvent>(OnResetClicked);
   }
 
   private void OnDisable() {
+    resetButton.UnregisterCallback<ClickEvent>(OnResetClicked);
     stopButton.UnregisterCallback<ClickEvent>(OnStopClicked);
     clipButton.UnregisterCallback<ClickEvent>(OnClipClicked);
     startButton.UnregisterCallback<ClickEvent>(OnStartClicked);
+    initializeButton.UnregisterCallback<ClickEvent>(OnInitializeClicked);
   }
 
-  private void OnStopClicked(ClickEvent evt) {
-    var element = (VisualElement)evt.target;
-    Debug.Log($"{element.name} was clicked!");
+  private void OnInitializeClicked(ClickEvent clickEvent) {
+    before.style.display = new StyleEnum<DisplayStyle>(StyleKeyword.None);
+    after.style.display = new StyleEnum<DisplayStyle>(StyleKeyword.Initial);
+    string message = "Initialized";
+    if (useBackgroundModeToggle.value) {
+      message += " in background mode";
+    }
+    Debug.Log(message);
   }
 
-  private void OnClipClicked(ClickEvent evt) {
-    var element = (VisualElement)evt.target;
-    Debug.Log($"{element.name} was clicked!");
+  private void OnStartClicked(ClickEvent clickEvent) {
+    statusLabel.text = "Streaming";
+    string message = "Started streaming ";
+    if (string.IsNullOrEmpty(eventInput.text)) {
+      message += "default event";
+    } else {
+      message += $"event \"{eventInput.text}\"";
+    }
+    if (useCameraToggle.value) {
+      message += " with camera";
+    }
+    if (useMicrophoneToggle.value) {
+      message += " with microphone";
+    }
+    Debug.Log(message);
   }
 
-  private void OnStartClicked(ClickEvent evt) {
-    var element = (VisualElement)evt.target;
-    Debug.Log($"{element.name} was clicked!");
+  private void OnClipClicked(ClickEvent clickEvent) {
+    Debug.Log("Created clip");
   }
 
-  public static void OnEventChanged(ChangeEvent<string> evt) {
-    Debug.Log($"{evt.newValue} -> {evt.target}");
+  private void OnStopClicked(ClickEvent clickEvent) {
+    statusLabel.text = "Idle";
+    string message = "Stopped streaming ";
+    if (string.IsNullOrEmpty(eventInput.text)) {
+      message += "default event";
+    } else {
+      message += $"event \"{eventInput.text}\"";
+    }
+    Debug.Log(message);
+  }
+
+  private void OnResetClicked(ClickEvent clickEvent) {
+    after.style.display = new StyleEnum<DisplayStyle>(StyleKeyword.None);
+    before.style.display = new StyleEnum<DisplayStyle>(StyleKeyword.Initial);
   }
 }
